75ab69b012b4a90f874d93491ff6dbe1297c82fa
Ikko-T <skyblueboy32@gmail.com>
2021-11-19 21:03:19 +0900

    Set up a database connection.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/cookies'
+require 'pg'
+require 'pry'
+
+agent = PG::connect(
+  :host => ENV.fetch("DB_HOST", "localhost"),
+  :user => ENV.fetch("DB_USER"),
+  :password => ENV.fetch("DB_PASSWORD"),
+  :dbname => ENV.fetch("DB_NAME" )
+)
 
 get '/posts' do
   return erb :posts
