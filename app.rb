f72a9173f309ad88666a0b4d78dbc9afa8dbfafb
Ikko-T <skyblueboy32@gmail.com>
2021-11-20 15:04:56 +0900

    Display a posting list.

--- app.rb
+++ app.rb
@@ -11,19 +11,21 @@ agent = PG::connect(
   :dbname => ENV.fetch("DB_NAME" )
 )
 
+# 投稿画面へ遷移
 get '/posts' do
+  # データの参照
+  @posts = agent.exec_params("SELECT * FROM posts ORDER BY created_at DESC").to_a
   return erb :posts
 end
 
+# 投稿画面
 post '/posts' do
+  # 入力データの取得
   name = params[:name]
   content = params[:content]
 
+  # データの登録
   agent.exec_params("INSERT INTO posts (name, content) VALUES ($1, $2)", [name, content])
-
-  @post = agent.exec_params("SELECT * FROM posts WHERE name = $1 AND content = $2", [name, content]).to_a.first
-
+  # binding.pry
   return redirect '/posts'
 end
-
-# binding.pry
